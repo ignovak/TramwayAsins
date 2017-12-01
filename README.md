628e4c09521906d303d329d8be54b93bb00af89f
Igor Novak <bezengi@gmail.com>
2017-12-01 05:03:38 +0100

    Add /features.html page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Source code for the static website [sapp/TramwayAsins](https://sapp.amazon.com/TramwayAsins) hosting ASIN data for Tramway project
+
+[features.html](https://sapp.amazon.com/TramwayAsins/features.html) – list of features available for specific ASINs
---
+++ webapp/features.js
@@ -0,0 +1,43 @@
+let asins;
+
+const app = new Vue({
+  el: '#app',
+  data: {
+      columns: [],
+      host: location.hash.includes('devo') ? 'https://tr-development.amazon.com/dp/' : 'https://tr-pre-prod.amazon.com/dp/',
+      asins: [],
+      filters: []
+  },
+  methods: {
+    update: function() {
+      this.asins = asins.filter(_ => this.filters.every(feature => _.features.has(feature)));
+    }
+  }
+});
+
+new Promise(function(resolve, reject) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'features.json', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4)
+                resolve(xhr.responseText);
+        }
+        xhr.send();
+    })
+    .then(_ => {
+        const data = JSON.parse(_);
+        data.forEach(_ => {
+          _.features = new Set(_.features.map(_ => _.replace('_feature_div', '')));
+        });
+        const columns = new Set([].concat(...data.map(_ => [..._.features])));
+        columns.delete('hero-quick-promo-grid');
+        columns.delete('product-alert-grid');
+        columns.delete('qpe-title-tag');
+        app.columns = [...columns];
+        asins = data
+            .map(_ => {
+                _.title = _.url.match(/\w+$/)[0];
+                return _;
+            });
+        app.update();
+    });
